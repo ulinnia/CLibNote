7313e79d0be35f884f740db48508d68a8715b912
ulinnia <ryan98244@gmail.com>
2023-11-29 01:01:26 +0000

    XOR twice

--- Exchange.c
+++ Exchange.c
@@ -1,11 +1,19 @@
 #include<stdio.h>
 
-int main(){
+void Exchange(){
     char a,b;
     scanf("%hhd%hhd",&a,&b);
     a = a^b;
     b = a^b;
     a = a^b;
     printf("After exchange.............\na = %hhd\nb = %hhd\n", a, b);
-    return 0;
+}
+
+int main(){
+    int a = 5428;
+    a ^= 123;
+    printf("%d\n",a);
+    a ^= 123;
+    printf("%d\n",a);
+    return 0; 
 }
