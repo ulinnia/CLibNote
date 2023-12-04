cf4c3d7d4fb4a1cd6c1fa388d18db7fabbd159d7
ulinnia <ryan98244@gmail.com>
2023-12-04 07:10:39 +0000

    Stdio.h renew

--- Stdio.c
+++ Stdio.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int main(){
-	printf("Hello Word!!!\n");
+	FILE * pf = fopen("a.txt","a+");
+
+    if (fgetc(pf)==EOF){
+        printf("didn't get any words!!!\n");
+    }
+    fprintf(pf,"%s","xxxxxxxx");
+
+    fclose(pf);
+    return(0);
 }
